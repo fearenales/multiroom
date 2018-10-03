4f2ab47e414f0c285f5ee4efec776120eff33b3b
Felipe Santos <felipe.santos@pospulse.com>
2018-10-03 12:30:48 +0200

    Add /spotify/transfer endpoint

--- http/index.js
+++ http/index.js
@@ -1,27 +1,10 @@
-const request = require('request');
-const express = require('express');
-const app = express();
-const spawnSync = require( 'child_process' ).spawnSync;
+require('dotenv').config()
+
+const request       = require('request');
+const express       = require('express');
+const app           = express();
+const spawnSync     = require( 'child_process' ).spawnSync;
 const SpotifyWebApi = require('spotify-web-api-node');
-
-const credentials = {
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'https://example.com/callback'
-};
-
-const spotifyApi = new SpotifyWebApi(credentials);
-
-if (process.env.SPOTIFY_AUTH_CODE) {
-  spotifyApi.authorizationCodeGrant(process.env.SPOTIFY_AUTH_CODE).then((data) => {
-    console.log(data);
-    spotifyApi.setAccessToken(data.body['access_token']);
-    spotifyApi.setRefreshToken(data.body['refresh_token']);
-  })
-  .catch((err) => {
-    console.log('Something went wrong!', err);
-  });
-}
 
 const normalizeRoom = (room) => {
   return room.replace(/\b\w/g, l => l.toUpperCase());
@@ -37,7 +20,21 @@ app.get('/bluetooth/:action', function (req, res) {
   res.send(result);
 });
 
-app.get('/devices', function (req, res) {
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: 'https://example.com/callback'
+});
+
+if (process.env.SPOTIFY_ACCESS_TOKEN) {
+  spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
+}
+
+if (process.env.SPOTIFY_REFRESH_TOKEN) {
+  spotifyApi.setRefreshToken(process.env.SPOTIFY_REFRESH_TOKEN);
+}
+
+app.get('/spotify/devices', function (req, res) {
   spotifyApi.getMyDevices().then((response) => {
     res.send(response.body.devices);
   })
@@ -46,23 +43,53 @@ app.get('/devices', function (req, res) {
   });
 });
 
-
-app.get('/token', function(req, res) {
-  const scopes = ['user-read-playback-state'],
-    redirectUri = 'https://example.com/callback',
-    clientId = process.env.SPOTIFY_CLIENT_ID,
-    state = '';
-
-  // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-  var spotifyApi = new SpotifyWebApi({
-    redirectUri: redirectUri,
-    clientId: clientId
+const findSpotifyDevice = (deviceName) => {
+  return spotifyApi.getMyDevices().then((response) => {
+    return response.body.devices.find((device) => {
+      device.name.toUpperCase() === deviceName.toUpperCase();
+    });
   });
+}
 
-  // Create the authorization URL
-  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+app.get('/spotify/transfer', function (req, res) {
+  const deviceName = req.query.device;
+  const device = findSpotifyDevice(deviceName);
+  if (!device) {
+    return res.status(404).send(`Device ${deviceName} not found`);
+  }
+  spotifyApi.transferMyPlayback({
+    device_ids: [device.id],
+    play: true,
+  }).then(() => {
+    res.send('ok');
+  })
+  .catch((err) => {
+    res.send(err);
+  });
+});
 
-  // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+const writeEnv = (key, value) => {
+  require('fs').appendFile("./.env", `${key}=${value}\n`, function(err) {
+    if(!err) { return; }
+    console.err(err);
+  }); 
+}
+
+app.get('/spotify/setup', function(req, res) {
+  spotifyApi.authorizationCodeGrant(req.query.code).then((data) => {
+    writeEnv('SPOTIFY_ACCESS_TOKEN', data.body['access_token']);
+    writeEnv('SPOTIFY_REFRESH_TOKEN', data.body['refresh_token']);
+    spotifyApi.setAccessToken(data.body['access_token']);
+    spotifyApi.setRefreshToken(data.body['refresh_token']);
+    res.send('ok');
+  })
+  .catch((err) => {
+    res.send(err);
+  });
+});
+
+app.get('/spotify/token', function(req, res) {
+  const authorizeURL = spotifyApi.createAuthorizeURL(['user-read-playback-state'], '');
   res.send(authorizeURL);
 });
 
